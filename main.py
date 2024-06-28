d0daf86a9f6aea25f94dbe18bcb7c14f116fd988
Luiz Fernando <luizfgrocha04@gmail.com>
2024-06-28 18:07:21 -0300

    Adicionadas anotações de tipo

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 import sys
+from typing import List
 
 
 class Point:
 
-  def __init__(self, x, y):
-    self.x = x
-    self.y = y
+  def __init__(self, x: float, y: float):
+    self.x: float = x
+    self.y: float = y
 
 
 class Polygon:
 
+  points: List[Point]
+
   def __init__(self):
     self.points = []
 
-  def add_point(self, i_point: Point):
+  def add_point(self, i_point: Point) -> None:
     self.points.append(i_point)
 
-  def remove_point(self, i_point: Point):
+  def remove_point(self, i_point: Point) -> None:
     self.points.remove(i_point)
 
-  def remove_point(self, index: int):
+  def remove_point_at(self, index: int):
     self.points.pop(index)
 
 
-def parse_input(filename: str):
+def parse_input(filename: str) -> Polygon:
   polygon = Polygon()
 
   with open(filename, 'r') as file:
@@ -41,12 +44,18 @@ def parse_input(filename: str):
 
   return polygon
 
+
+def triangulate(polygon: Polygon) -> Polygon:
+  pass
+
+
 def main():
   filename = sys.argv[1]
   polygon = parse_input(filename)
 
   for point in polygon.points:
     print(f'({point.x}, {point.y})')
+    
 
 if __name__ == '__main__':
   main()
